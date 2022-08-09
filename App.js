92e4198417b97f349aa025188d7b8eb4e9eef7ff
Jan-far <ligalifarouk@yahoo.com>
2022-08-09 13:34:34 +0100

    feat: Complete Android implementation

--- App.js
+++ App.js
@@ -6,107 +6,64 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import MiSnapManager from 'react-native-misnap';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
+function App() {
   const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  const [captureType, setCaptureType] = useState('');
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    const config = {
+      captureType: captureType,
+      autocapture: true,
+      livenessLicenseKey: 'MISNAP_LIVENESS_LICENSE_KEY',
+      glare: 0.5,
+      imageQuality: 1.0,
+      contrast: 0.5,
+    };
+
+    MiSnapManager.capture(config)
+      .then(result => {
+        const capturedImage = result.base64encodedImage;
+        console.log('result', capturedImage);
+        const {metadata = {}} = result;
+        console.log(metadata);
+        // Do something with base64 encoded image string and optional metaData
+      })
+      .catch(error => {
+        console.log('error', error);
+        // Do something with error
+      });
+  }, [captureType]);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+        <Text style={{fontSize: 24, fontWeight: 'bold'}}>Capture Types</Text>
+        {['idFront', 'idBack', 'face'].map((type, i) => (
+          <View key={i} style={{marginVertical: 15, width: 100}}>
+            <Button onPress={() => setCaptureType(type)} title={type} />
+          </View>
+        ))}
+      </View>
     </SafeAreaView>
   );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+}
 
 export default App;
